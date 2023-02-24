b74b5968b299331e36e9f9355bdd964f8e1f3b01
PrimeCoder05 <lukas.a.pena@gmail.com>
2023-02-24 18:43:51 -0500

    change the color of the submit button

--- src/components/Submit.js
+++ src/components/Submit.js
@@ -3,7 +3,7 @@ function Submit() {
 		<>
 			<label htmlFor="details">ADDITIONAL DETAILS</label><br/>
 			<textarea name="additional-details" id="details" placeholder="(optional) Please tell me more details about your event."></textarea> <br/><br/>
-        	<input type="submit" value="Submit" />
+        	<input type="submit" className="submit" value="SUBMIT" />
 		</>
 	);
 }
