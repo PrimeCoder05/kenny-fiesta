29d5fe712c04f70fc9fdca205782b6426b8353df
PrimeCoder05 <lukas.a.pena@gmail.com>
2023-02-28 17:46:02 -0500

    make sure code is nested properly

--- src/components/DateAndTime.js
+++ src/components/DateAndTime.js
@@ -2,7 +2,7 @@ function DateAndTime() {
 	return (
 		<>
 			<label htmlFor="event-date">DATE OF EVENT</label> <span className="required">*</span><br/>
-         <input type="date" name="event-date" id="event-date" required /> <br/><br/>
+         	<input type="date" name="event-date" id="event-date" required /> <br/><br/>
 
 			<label htmlFor="event-time">TIME OF EVENT</label> <span className="required">*</span><br/>
 			<input type="time" name="event-time" id="event-time" required /> <br/><br/>
