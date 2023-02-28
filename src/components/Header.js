4b8f606c5b7084e6bde95c0881ac08a0deb0231d
PrimeCoder05 <lukas.a.pena@gmail.com>
2023-02-28 13:09:21 -0500

    add white hamburger and background image for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 function Header() {
 	return (
 		<>
-			<div className="header">
+			<div className="header" style={{ backgroundImage: `url(./Hero-Image.jpg)` }}>
 				<nav>
 					<ul>
 						<li className="logo">Kenny Fiesta</li>
-						<li><img src="./Hamburger_icon.svg.png" alt="Other options" width={40} /></li>
+						<li><img src="./icons8-menu-rounded-50.png" alt="Other options" width={40} /></li>
 						{/* <li>HOME</li>
 						<li>ABOUT</li>
 						<li>PRICE LIST</li>
