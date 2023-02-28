d4e80c77006106995d0b2bcb87cbdc64ba51a61f
PrimeCoder05 <lukas.a.pena@gmail.com>
2023-02-28 12:31:23 -0500

    add hamburger icon for dropdown list and remove unnecessary css

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,15 @@ function Header() {
 	return (
 		<>
 			<div className="header">
-				{/* <img src="./Hamburger_icon.svg.png" alt="Other options" /> */}
 				<nav>
 					<ul>
 						<li className="logo">Kenny Fiesta</li>
-						<li>HOME</li>
+						<li><img src="./Hamburger_icon.svg.png" alt="Other options" width={40} /></li>
+						{/* <li>HOME</li>
 						<li>ABOUT</li>
 						<li>PRICE LIST</li>
 						<li>FAQS</li>
-						<li>CONTACT</li>
+						<li>CONTACT</li> */}
 					</ul>
 				</nav>
 				<h1>Contact</h1>
