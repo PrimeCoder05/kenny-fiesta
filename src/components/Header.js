98a507eba94cf2ad2e0e0bbbbfe9ab2789bbb9c4
PrimeCoder05 <lukas.a.pena@gmail.com>
2023-03-09 17:43:12 -0500

    rework the navbar to be more responsive and update css to incorporate better use of flex

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,18 @@ function Header() {
 		<>
 			<div className="header" style={{ backgroundImage: `url(./Hero-Image.jpg)` }}>
 				<nav>
-					<ul>
-						<li className="logo">Kenny Fiesta</li>
-						<li><img src="./icons8-menu-24.png" alt="Other options" width={30} /></li>
-						{/* <li>HOME</li>
-						<li>ABOUT</li>
-						<li>PRICE LIST</li>
-						<li>FAQS</li>
-						<li>CONTACT</li> */}
-					</ul>
+					<a href="index.html" className="logo">Kenny Fiesta</a>
+
+					<div>
+						<ul className="navbar">
+							{/* <li><img src="./icons8-menu-24.png" alt="Other options" width={30} /></li> */}
+							<li><a href="index.html">HOME</a></li>
+							<li><a href="index.html">ABOUT</a></li>
+							<li><a href="index.html">PRICE LIST</a></li>
+							<li><a href="index.html">FAQS</a></li>
+							<li><a href="index.html">CONTACT</a></li>
+						</ul>
+					</div>
 				</nav>
 				<h1>Contact</h1>
 			</div>
